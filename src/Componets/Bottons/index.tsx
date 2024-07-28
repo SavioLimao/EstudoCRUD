a3930abc4aa8988500bbb39f4697984adb9945cf
Sávio Limão <saviolimao@hotmail.com>
2024-07-28 11:47:30 -0300

    Iniciando implementacao State

--- src/Componets/Bottons/index.tsx
+++ src/Componets/Bottons/index.tsx
@@ -1,37 +1,30 @@
-import { BTN } from "../Button"
-import { EditIcon } from "../Edit Icon"
-
+import { BTN } from "../Button";
+import { EditIcon } from "../Edit Icon";
 
 export const Buttons = () => {
   const Plus = () => {
-    console.log('plus')
-  }
+    console.log("plus");
+  };
   const Edit = () => {
-    console.log('Edit')
-  }
+    console.log("Edit");
+  };
   const Del = () => {
-    console.log('Del')
-  }
+    console.log("Del");
+  };
 
-
-
-    return (
-        <aside>
-                <div className="Plus">
-                  <BTN onClick={Plus}>
-                    +
-                  </BTN>
-                </div>
-                <div className="Edit">
-                  <BTN onClick={Edit}>
-                    <EditIcon />
-                  </BTN>
-                </div>
-                <div className="Delete">
-                  <BTN onClick={Del}>
-                    -
-                  </BTN>
-                </div>
-        </aside>
-    )
-}
+  return (
+    <aside>
+      <div className="Plus">
+        <BTN onClick={Plus}>+</BTN>
+      </div>
+      <div className="Edit">
+        <BTN onClick={Edit}>
+          <EditIcon />
+        </BTN>
+      </div>
+      <div className="Delete">
+        <BTN onClick={Del}>-</BTN>
+      </div>
+    </aside>
+  );
+};
