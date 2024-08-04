04cbcba95d69d7f85c212e5cd5b37cea146b020b
Sávio Limão <saviolimao@hotmail.com>
2024-08-04 16:43:02 -0300

    comecando botoes

--- src/Componets/Main/index.tsx
+++ src/Componets/Main/index.tsx
@@ -2,36 +2,39 @@ import { useEffect, useState } from "react";
 import { Buttons } from "../Bottons";
 import { api } from "../../API/api";
 
-
-const Main = () => {
-  const [Produtos, setProdutos] = useState([
-    
-  ]);
-
-  const AddProduct = () => {
-    const NewProduct = {
-      id: Produtos.length + 1,
-      Nome: "Novo",
-      Descricao: "Nova Descrição",
-      Preco: 0.0,
-      Estoque: 0,
-    };
-    setProdutos([...Produtos, NewProduct]);
-  };
+type Produto = {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+}
+export const Main = () => {
+  const [Produtos, setProdutos] = useState([]);
+  useEffect(() => {
+    getPosts()
+  }, []);
 
   const getPosts = async () => {
     try {
-      
-      const response = await axios.get()
-      
+      const responseAxios = await api.get("/produtos");
+      const produtoApi = responseAxios.data;
+      setProdutos(produtoApi)
     } catch (error) {
-      alert('deu')
+      alert("deu pau");
     }
-  }
+  };
 
-  useEffect(() => {
+  // const AddProduct = () => {
+  //   const NewProduct = {
+  //     id: Produtos.length + 1,
+  //     Nome: "Novo",
+  //     Descricao: "Nova Descrição",
+  //     Preco: 0.0,
+  //     Estoque: 0,
+  //   };
+  // };
 
-  }, [])
+  
   return (
     <div className="MainDiv">
       <table>
@@ -48,17 +51,18 @@ const Main = () => {
           </th>
         </tr>
         <tbody>
-          {Produtos.map((Produto) => (
-            <tr>
+          {Produtos.map((Produto, index) => (
+            <tr key={index}>
               <td>{Produto.Nome}</td>
               <td>{Produto.Descricao}</td>
               <td>R$ {Produto.Preco}</td>
               <td>{Produto.Estoque}</td>
-              <Buttons AddProduct={AddProduct} />
+              <Buttons />
             </tr>
           ))}
         </tbody>
       </table>
     </div>
   );
+  
 };
